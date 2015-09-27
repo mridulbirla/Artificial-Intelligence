960fdf903785aed230e77a652c387b1c311c8026
Mridul Birla <mbirla@iu.edu>
2015-09-27 02:03:10 -0400

    Tree with BFS, Maximum difference and lowest child functionality

--- Assignment2/Node.py
+++ Assignment2/Node.py
@@ -16,5 +16,19 @@ class Node:
     def get_children(self):
         return self.children
 
+    def sort_children(self):
+        self.children.sort(key=lambda x: x.val, reverse=False)
 
+'''
+Just For Testing Purpose only
+n=Node(4)
+n1=Node(5)
+n2=Node(6)
+n3=Node(7)
+n.add_children(n2)
+n.add_children(n1)
+n.add_children(n3)
+n.sort_children()
+print n
 
+'''
